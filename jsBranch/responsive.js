7fba680f5dc61f3d6882535f4c41b499cc8bbffd
bedstop <service@bedstop.de>
2017-02-25 02:28:29 +0100

    Had no onload, solved

--- jsBranch/responsive.js
+++ jsBranch/responsive.js
@@ -1,34 +1,41 @@
-var nav=document.getElementById('nav');
-var titleImg=document.getElementById('titleImg');
-var titleImgWidth=window.innerWidth-15 +'px';
-var navHeight=39;
-var padTitleImg=15;
-var ogWidth=5530;
-var ogHeight=1277;
-var widthRatio=titleImgWidth.match(/\d+/)[0]/ogWidth;
-titleImgHeight=ogHeight*widthRatio;
 
-titleImg.style.width=titleImgWidth;
-nav.style.top=(titleImgHeight+padTitleImg-navHeight)/1.2+'px';
-var navYPos=(titleImgHeight+padTitleImg-navHeight)/1.2;
-var navNewYPos=titleImgHeight+padTitleImg;
-//console.log(titleImgHeight+padTitleImg-navYPos);
+function init() {
+    var nav=document.getElementById('nav');
+    var titleImg=document.getElementById('titleImg');
+    var titleImgWidth=window.innerWidth-15 +'px';
+    var navHeight=39;
+    var padTitleImg=15;
+    var ogWidth=5530;
+    var ogHeight=1277;
+    var widthRatio=titleImgWidth.match(/\d+/)[0]/ogWidth;
+    titleImgHeight=ogHeight*widthRatio;
 
-
-window.onresize= () => {
-  var titleImgWidth = window.innerWidth-15 +'px'
-  var widthRatio=titleImgWidth.match(/\d+/)[0]/ogWidth
-  titleImgHeight=ogHeight*widthRatio
-
-  titleImg.style.width=titleImgWidth
-  nav.style.top=(titleImgHeight+padTitleImg-navHeight)/1.2+'px'
-  navYPos=(titleImgHeight+padTitleImg-navHeight)/1.2
-  navNewYPos=titleImgHeight+padTitleImg
-  console.log(titleImgHeight+padTitleImg-navYPos)
+    titleImg.style.width=titleImgWidth;
+    nav.style.top=(titleImgHeight+padTitleImg-navHeight)/1.2+'px';
+    var navYPos=(titleImgHeight+padTitleImg-navHeight)/1.2;
+    var navNewYPos=titleImgHeight+padTitleImg;
+    //console.log(titleImgHeight+padTitleImg-navYPos);
 }
 
+window.onload = init;
+window.onresize = init;
 
 document.getElementById('fullPage').onscroll = () => {
+    var nav=document.getElementById('nav');
+    var titleImg=document.getElementById('titleImg');
+    var titleImgWidth=window.innerWidth-15 +'px';
+    var navHeight=39;
+    var padTitleImg=15;
+    var ogWidth=5530;
+    var ogHeight=1277;
+    var widthRatio=titleImgWidth.match(/\d+/)[0]/ogWidth;
+    titleImgHeight=ogHeight*widthRatio;
+
+    titleImg.style.width=titleImgWidth;
+    nav.style.top=(titleImgHeight+padTitleImg-navHeight)/1.2+'px';
+    var navYPos=(titleImgHeight+padTitleImg-navHeight)/1.2;
+    var navNewYPos=titleImgHeight+padTitleImg;
+    
     var fullPage = document.getElementById('fullPage');
     if (fullPage.scrollTop>titleImgHeight+padTitleImg-navYPos && fullPage.scrollTop<titleImgHeight+padTitleImg){
       nav.style.position="absolute";
